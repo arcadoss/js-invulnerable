95be5ddcd5ab97bd2751461609d7441cd335ed1c
Sergey Dovgal <arcadoss-f@ya.ru>
2010-12-06 18:27:22 +0300

    Compiler options filled with new values

---
+++ src/com/google/javascript/jscomp/MyPassConfig.java
@@ -0,0 +1,11 @@
+package com.google.javascript.jscomp;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: arcadoss
+ * Date: 06.12.10
+ * Time: 12:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyPassConfig {
+}
