71e47c7c3d48e5c18373a9b54e6006b30c96d406
Sergey Dovgal <arcadoss-f@ya.ru>
2011-02-16 13:54:34 +0300

    Changed default analyzer's optoins

--- src/com/google/javascript/jscomp/MyCommandLineRunner.java
+++ src/com/google/javascript/jscomp/MyCommandLineRunner.java
@@ -10,40 +10,75 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class MyCommandLineRunner extends CommandLineRunner {
-    public MyCommandLineRunner(String[] args) {
-        super(args);
 
-        CommandLineConfig commandLineConfig = getCommandLineConfig();
+  public MyCommandLineRunner(String[] args) {
+    super(args);
 
-        commandLineConfig.setPrintAst(true);
+  }
+
+  @Override
+  protected Compiler createCompiler() {
+    Compiler compiler = new Compiler(getErrorPrintStream());
+    PassConfig myPasses = new DefaultPassConfig(createOptions());
+
+    compiler.setPassConfig(myPasses);
+    return compiler;
+  }
+
+  @Override
+  protected CompilerOptions createOptions() {
+    CompilerOptions myCoolOptions = new CompilerOptions();
+
+//        myCoolOptions.skipAllPasses = false;
+//        myCoolOptions.nameAnonymousFunctionsOnly = false;
+//        myCoolOptions.devMode = DevMode.OFF;
+    myCoolOptions.checkSymbols = true;
+    myCoolOptions.checkShadowVars = CheckLevel.WARNING;
+    myCoolOptions.aggressiveVarCheck = CheckLevel.WARNING;
+    myCoolOptions.checkFunctions = CheckLevel.WARNING;
+    myCoolOptions.checkMethods = CheckLevel.WARNING;
+//        myCoolOptions.checkDuplicateMessages = false;
+//        myCoolOptions.allowLegacyJsMessages = false;
+//        myCoolOptions.strictMessageReplacement = false;
+    myCoolOptions.checkSuspiciousCode = true;
+    myCoolOptions.checkControlStructures = true;
+//        myCoolOptions.checkUndefinedProperties = CheckLevel.OFF;
+//        myCoolOptions.checkUnusedPropertiesEarly = false;
+    myCoolOptions.checkTypes = true;
+    myCoolOptions.tightenTypes = true;
+    myCoolOptions.inferTypesInGlobalScope = true;
+    myCoolOptions.checkTypedPropertyCalls = true;
+//        myCoolOptions.reportMissingOverride = CheckLevel.OFF;
+    myCoolOptions.reportUnknownTypes = CheckLevel.WARNING;
+//        myCoolOptions.checkRequires = CheckLevel.OFF;
+//        myCoolOptions.checkProvides = CheckLevel.OFF;
+//        myCoolOptions.checkGlobalNamesLevel = CheckLevel.OFF;
+//        myCoolOptions.brokenClosureRequiresLevel = CheckLevel.ERROR;
+//        myCoolOptions.checkGlobalThisLevel = CheckLevel.OFF;
+    myCoolOptions.checkUnreachableCode = CheckLevel.WARNING;
+//        myCoolOptions.checkMissingReturn = CheckLevel.OFF;
+//        myCoolOptions.checkMissingGetCssNameLevel = CheckLevel.OFF;
+//        myCoolOptions.checkMissingGetCssNameBlacklist = null;
+//        myCoolOptions.checkEs5Strict = false;
+//        myCoolOptions.checkCaja = false;
+    myCoolOptions.computeFunctionSideEffects = true;
+    myCoolOptions.chainCalls = true;
+
+
+    myCoolOptions.ideMode = true;
+
+
+    return myCoolOptions;
+  }
+
+  public static void main(String[] args) {
+    MyCommandLineRunner runner = new MyCommandLineRunner(args);
+
+    if (runner.shouldRunCompiler()) {
+      runner.run();
+    } else {
+      System.exit(-1);
     }
-
-    @Override
-    protected Compiler createCompiler() {
-        Compiler compiler = new Compiler(getErrorPrintStream());
-        PassConfig myPasses = new DefaultPassConfig(createOptions());
-
-        compiler.setPassConfig(myPasses);
-        return compiler;
-    }
-
-    @Override
-    protected CompilerOptions createOptions() {
-        CompilerOptions myCoolOptions = new CompilerOptions();
-
-        myCoolOptions.ideMode = true;
-
-        return myCoolOptions;
-    }
-
-    public static void main(String[] args) {
-        MyCommandLineRunner runner = new MyCommandLineRunner(args);
-
-        if (runner.shouldRunCompiler()) {
-            runner.run();
-        } else {
-            System.exit(-1);
-        }
-    }
+  }
 
 }
--- src/com/google/javascript/jscomp/MyPassConfig.java
+++ src/com/google/javascript/jscomp/MyPassConfig.java
@@ -1,5 +1,11 @@
 package com.google.javascript.jscomp;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.javascript.rhino.Node;
+
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: arcadoss
@@ -7,5 +13,78 @@ package com.google.javascript.jscomp;
  * Time: 12:45
  * To change this template use File | Settings | File Templates.
  */
-public class MyPassConfig {
+public class MyPassConfig extends PassConfig {
+    private PassFactory inferTypes = new PassFactory("inferTypes", false) {
+        @Override
+        protected CompilerPass createInternal(final AbstractCompiler compiler) {
+            return new CompilerPass() {
+                @Override
+                public void process(Node externs, Node root) {
+                    Preconditions.checkNotNull(topScope);
+                    Preconditions.checkNotNull(typedScopeCreator);
+
+                    makeTypeInference(compiler).process(externs, root);
+                }
+            };
+        }
+    };
+    private PassFactory resolveTypes = new PassFactory("resolveTypes", false) {
+        @Override
+        protected CompilerPass createInternal(AbstractCompiler compiler) {
+            return new GlobalTypeResolver(compiler);
+        }
+    };
+
+    public MyPassConfig(CompilerOptions options) {
+        super(options);
+    }
+
+    @Override
+    protected List<PassFactory> getChecks() {
+        List<PassFactory> checks = Lists.newArrayList();
+
+        checks.add(inferTypes.makeOneTimePass());
+        checks.add(resolveTypes.makeOneTimePass());
+
+        return checks;
+    }
+
+    /**
+     * We don't want ot optimize code. We are trying to analyze it.
+     *
+     * @return null
+     */
+    @Override
+    protected List<PassFactory> getOptimizations() {
+        return null;
+    }
+
+    @Override
+    State getIntermediateState() {
+        return null;
+    }
+
+    @Override
+    void setIntermediateState(State state) {
+
+    }
+
+    private class GlobalTypeResolver implements CompilerPass {
+        private final AbstractCompiler compiler;
+
+        GlobalTypeResolver(AbstractCompiler compiler) {
+            this.compiler = compiler;
+        }
+
+        @Override
+        public void process(Node externs, Node root) {
+            if (topScope == null) {
+                typedScopeCreator =
+                        new MemoizedScopeCreator(new TypedScopeCreator(compiler));
+                topScope = typedScopeCreator.createScope(root.getParent(), null);
+            } else {
+                compiler.getTypeRegistry().resolveTypesInScope(topScope);
+            }
+        }
+    }
 }
