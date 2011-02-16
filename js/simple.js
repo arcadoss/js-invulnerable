29226c209db05f4662388124ad4f0e4d8a73200b
Sergey Dovgal <arcadoss-f@ya.ru>
2011-02-16 13:41:42 +0300

    JavaScript examples added

--- js/simple.js
+++ js/simple.js
@@ -4,7 +4,26 @@ global = 10;
 function fun1 (a, b) {
   var local = a;
   global = b;
-  return;
+  a = -b;
+
+  c = (a > 0 ? 10 : "string");
+  return c;
 }
 
 fun1(10, 2);
+
+this["prop"] = 10;
+
+if (10 < 2) {
+  global += 1
+}
+
+if (7 > 2) {
+  global -= 2
+} else {
+  this["prop2"] = this["prop"]
+}
+
+for (var i = 0; i < 10; ++i) {
+  global /= i
+}
