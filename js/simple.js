cbab8632eac85ab5e69e29a4ebbc457af23048bd
Sergey Dovgal <arcadoss-f@ya.ru>
2011-02-17 15:32:22 +0300

    sample JS file changed

--- js/simple.js
+++ js/simple.js
@@ -1,5 +1,6 @@
 
 global = 10;
+var q, w, e = 'str', r = 10;
 
 function fun1 (a, b) {
   var local = a;
@@ -26,4 +27,27 @@ if (7 > 2) {
 
 for (var i = 0; i < 10; ++i) {
   global /= i
+}
+
+while (global > 10) {
+  global -= 1
+}
+
+do {
+  gobal += 1
+} while (global < 0)
+
+while (True) {
+  global += 1
+}
+
+switch (global) {
+  case 10:
+    a = 2;
+    break;
+  case 'asdf':
+    b = 3;
+    break;
+  default:
+    break;
 }
